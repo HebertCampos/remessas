11a326cbab56e2d9ef6228e34f21bbeeb2d09cf5
Hebert <hebertcdev@gmail.com.br>
2024-10-15 14:06:24 -0300

    Listando associados

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:remessas/models/associados_model.dart';
+import 'package:remessas/services/api_services.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -8,12 +10,82 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  ApiServices apiServices = ApiServices();
+  List<AssociadosModel> associados = [];
+  List<AssociadosModel> filteredAssociados = [];
+  bool _isLoading = true;
+  TextEditingController searchController = TextEditingController();
+
+  @override
+  void initState() {
+    super.initState();
+    fetchPacientes();
+  }
+
+  void fetchPacientes() async {
+    try {
+      List<AssociadosModel> fetchAssociados = await apiServices.getAssociados();
+      setState(() {
+        associados = fetchAssociados;
+        filteredAssociados = fetchAssociados;
+        _isLoading = false;
+      });
+    } catch (e) {
+      setState(() {
+        _isLoading = false;
+      });
+      throw 'Erro ao carregar associados: $e';
+    }
+  }
+
+  void filterSeach(String query) {
+    if (query.isNotEmpty) {
+      List<AssociadosModel> filteredList = associados
+          .where((associados) =>
+              associados.nome.toLowerCase().contains(query.toLowerCase()))
+          .toList();
+      setState(() {
+        filteredAssociados = filteredList;
+      });
+    } else {
+      setState(() {
+        filteredAssociados = associados;
+      });
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Sócios Contribuintes'),
         centerTitle: true,
+      ),
+      body: Stack(
+        children: [
+          if (_isLoading)
+            const Center(
+              child: CircularProgressIndicator(),
+            )
+          else
+            Column(
+              children: [
+                TextField(
+                  controller: searchController,
+                  onChanged: (value) => filterSeach(value),
+                  decoration:
+                      const InputDecoration(labelText: 'Buscar associado'),
+                ),
+                Expanded(
+                    child: ListView.builder(
+                        itemCount: filteredAssociados.length,
+                        itemBuilder: (context, index) {
+                          final associado = filteredAssociados[index];
+                          return Text(associado.nome);
+                        }))
+              ],
+            )
+        ],
       ),
     );
   }
--- lib/services/api_services.dart
+++ lib/services/api_services.dart
@@ -7,17 +7,18 @@ import 'package:http/http.dart' as http;
 class ApiServices {
   Future<List<AssociadosModel>> getAssociados() async {
     try {
-      final response =
-          await http.get(Uri.parse('$baseUrl/social/beneficiariosassociados'));
+      final response = await http.get(Uri.parse(
+          '$baseUrl/social/beneficiariosassociados?timestamp=${DateTime.now().millisecondsSinceEpoch}'));
       if (response.statusCode == 200) {
         List<dynamic> data = json.decode(response.body);
+        // print('RETORNO DAS INFORMAÇÕES: $data');
         return data.map((item) => AssociadosModel.fromJson(item)).toList();
       } else {
         final errorMessage = json.decode(response.body)['error'];
-        throw Exception('Erro: $errorMessage');
+        throw Exception('Erro 1: $errorMessage');
       }
     } catch (e) {
-      throw Exception('Erro: $e');
+      throw Exception('Erro 2: $e');
     }
   }
 }
