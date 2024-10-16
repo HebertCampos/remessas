e7494ccb72b4c38ad5120b5700e7b8351416860f
Hebert <hebertcdev@gmail.com.br>
2024-10-16 15:20:05 -0300

    Criando o rodape com a mudança de paginas

---
+++ lib/pages/components/footer_home_component.dart
@@ -0,0 +1,53 @@
+import 'package:flutter/material.dart';
+
+class FooterComponet extends StatelessWidget {
+  final int page; // Tornar 'page' um campo final para ser imutável
+
+  const FooterComponet({
+    super.key,
+    required this.page, // Passar 'page' como parâmetro requerido
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 50,
+      alignment: Alignment.bottomCenter,
+      child: SingleChildScrollView(
+        scrollDirection: Axis.horizontal,
+        child: Row(
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: ElevatedButton(
+                onPressed: page == 1 ? null : () {}, // Desativa se page == 1
+                child: const Text('Lista Sócios'),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: ElevatedButton(
+                onPressed: page == 2 ? null : () {}, // Desativa se page == 2
+                child: const Text('Data de Emissão'),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: ElevatedButton(
+                onPressed: page == 3 ? null : () {}, // Desativa se page == 3
+                child: const Text('Provisão'),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: ElevatedButton(
+                onPressed: page == 4 ? null : () {}, // Desativa se page == 4
+                child: const Text('Remessa'),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
