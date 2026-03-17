1984a52aa45aec68f66c33da48e8520a596d229c
Hebert Campos A <hebertcdev@gmail.com>
2026-03-17 08:41:10 -0300

    v 1.1.5 adicioando versao rodape

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -195,7 +195,7 @@ class _HomePageState extends State<HomePage> {
                     ),
                   ),
                   const FooterComponet(page: 1),
-                  // const Text('by Hebert Campos (DTI-ASFAL) - V 1.1.5', style: TextStyle(fontSize: 8, color: Colors.grey)),
+                  const Text('by Hebert Campos (DTI-ASFAL) - V 1.1.5', style: TextStyle(fontSize: 8, color: Colors.grey)),
                 ],
               ),
           ],
